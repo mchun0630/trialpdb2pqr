3c94568bfa65b52dba71670441bebbc23a348f77
mchun0630 <minju.chun@pnnl.gov>
2014-09-17 15:34:19 -0700

    new js pz

--- _includes/Picture_zoom/pictures_zoom.js
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<link rel="stylesheet" href="/scripts/picture-zoom/css/dg-picture-zoom.css" type="text/css" />
-<script src="/scripts/picture-zoom/js/external/mootools-1.2.4-core-yc.js"></script>
-<script src="/scripts/picture-zoom/js/external/mootools-more.js"></script>
-<script src="/scripts/picture-zoom/js/dg-picture-zoom.js"></script>
-<script src="/scripts/picture-zoom/js/dg-picture-zoom-autoload.js"></script>
-</html>
